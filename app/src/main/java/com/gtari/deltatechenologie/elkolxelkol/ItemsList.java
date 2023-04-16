a5dda4c7dfda149a6d32efcae664b960a73bcb04
testNewGithub <testgithub2323@gmail.com>
2023-04-16 04:43:38 +0100

    Add all my files

---
+++ app/src/main/java/com/gtari/deltatechenologie/elkolxelkol/ItemsList.java
@@ -0,0 +1,13 @@
+package com.gtari.deltatechenologie.elkolxelkol;
+
+
+public class ItemsList {
+
+    public String Name;
+    public String Desc;
+    ItemsList(String Name,String Desc){
+        this.Name=Name;
+        this.Desc=Desc;
+    }
+
+}
